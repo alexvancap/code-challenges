de2e7931a0559fbb5ee1ba5bbd6ea5b6acb02180
alexvancap <alexvancap@gmail.com>
2020-02-23 17:11:36 +0100

    made answer better

--- isNDevidableByXAndY.js
+++ isNDevidableByXAndY.js
@@ -1,7 +1,4 @@
 // Create a function that checks if a number n is divisible by two numbers x AND y. All inputs are positive, non-zero digits.
 function isDivisible(n, x, y) {
-    let output = true
-    if ((n / x) % 1 !== 0 || (n / y) % 1 !== 0)
-      output = false
-    return output
+    return n % x === 0 && n % y === 0
   }
