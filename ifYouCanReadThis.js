34817c125fa49051a90a8286625af16f426ba4e3
alexvancap <alexvancap@gmail.com>
2020-03-02 12:59:28 +0100

    finished if you can read this

---
+++ ifYouCanReadThis.js
@@ -0,0 +1,32 @@
+let table = {
+    'A': 'Alfa',
+    'B': 'Bravo',
+    'C': 'Charlie',
+    'D': 'Delta',
+    'E': 'Echo',
+    'F': 'Foxtrot',
+    'G': 'Golf',
+    'H': 'Hotel',
+    'I': 'India',
+    'J': 'Juliett',
+    'K': 'Kilo',
+    'L': 'Lima',
+    'M': 'Mike',
+    'N': 'November',
+    'O': 'Oscar',
+    'P': 'Papa',
+    'Q': 'Quebec',
+    'R': 'Romeo',
+    'S': 'Sierra',
+    'T': 'Tango',
+    'U': 'Uniform',
+    'V': 'Victor',
+    'W': 'Whiskey',
+    'X': 'Xray',
+    'Y': 'Yankee',
+    'Z': 'Zulu',
+}
+
+function to_nato(words) {
+    return words.split('').filter(c => c !== ' ').map(c => table[c.toUpperCase()] || c).join(' ');
+}
